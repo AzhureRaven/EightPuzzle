3d881d7fa982c8a50d6b06d336a5b669d95a9734
AzhureRaven <abrahamarthurfendy@gmail.com>
2022-09-05 14:13:41 +0700

    State Class

---
+++ EightPuzzle/State.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightPuzzle
+{
+    class State
+    {
+        public State parent;
+        public int[,] num;
+
+        public State(State parent, int[,] num)
+        {
+            this.parent = parent;
+            this.num = num;
+        }
+    }
+}
